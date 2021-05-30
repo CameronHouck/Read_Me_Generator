ea1376cb163e840e45262453496621755de1018f
Cameron Houck <cameronhouck1@gmail.com>
2021-05-30 15:54:08 -0400

    adding require variables

---
+++ index.js
@@ -0,0 +1,4 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const questionBank = require("./utils/questionBank.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
