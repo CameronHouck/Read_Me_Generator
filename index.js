a881e152ff314fe7e2956a5e89259c80b6e410b3
Cameron Houck <cameronhouck1@gmail.com>
2021-05-30 16:06:22 -0400

    adding generatemarkdown to generate readme file

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const fs = require("fs");
 const questionBank = require("./utils/questionBank.js");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 const writeToFile = (userInput) => {
-    fs.writeFile("README.md", userInput, (error) =>
-      error ? console.log("Error!") : console.log("Success!")
-    );
-  };
+  fs.writeFile("README.md", userInput, (error) =>
+    error ? console.log("Error!") : console.log("Success!")
+  );
+};
 
-  const promptUser = () => {
-    return inquirer.prompt(questionBank.questionBank);
-  };
+const promptUser = () => {
+  return inquirer.prompt(questionBank.questionBank);
+};
+
+const init = () => {
+  promptUser().then((userInput) => {
+    const markdown = generateMarkdown.generateMarkdown(userInput);
+    writeToFile(markdown);
+  });
+};
+
+init();
