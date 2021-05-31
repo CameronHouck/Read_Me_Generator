b9ead8f64681beadb7f1d11c7e667f2652cf0d05
Cameron Houck <cameronhouck1@gmail.com>
2021-05-31 14:32:01 -0400

    adding render license and badge image according to user input

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,21 @@
+const renderLicenseBadge = (license) => {
+  let licenseMarkdownText;
+  switch (license) {
+    case "MIT License":
+      licenseMarkdownText =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "GNU General Public License v3.0":
+      licenseMarkdownText =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "Apache License 2.0":
+      licenseMarkdownText =
+        "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "None":
+      licenseMarkdownText = "";
+      break;
+  }
+  return licenseMarkdownText;
+};
